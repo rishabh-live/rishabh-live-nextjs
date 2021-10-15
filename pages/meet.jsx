ba07137197c8df88692057feeb9c8aad54c9ad5f
Rishabh <34997913+rishabh-live@users.noreply.github.com>
2021-10-15 18:32:26 +0530

    Added Meet

---
+++ pages/meet.jsx
@@ -0,0 +1,10 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+export default function Meet() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("https://meet.google.com/cio-havg-hse");
+  });
+}
